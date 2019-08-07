b5fbe2754a9461edcfcb9dc575cd2199b601b28d
Chris <ckeenan@starry.com>
2019-08-07 17:29:40 -0400

    Updated parameters string builders

--- src/commonMain/kotlin/com/chrynan/graphqlquerybuilder/ObjectGraphQLQueryFieldBuilder.kt
+++ src/commonMain/kotlin/com/chrynan/graphqlquerybuilder/ObjectGraphQLQueryFieldBuilder.kt
@@ -11,8 +11,18 @@ data class ObjectGraphQLQueryFieldBuilder<B : BaseGraphQLQueryBuilder>(
     internal fun build(): String = buildString {
         append(name)
 
-        for (parameter in parameters) {
-            append("${parameter.name} = ${parameter.value}")
+        if (parameters.isNotEmpty()) {
+            append("(")
+
+            parameters.forEachIndexed { index, parameter ->
+                append("${parameter.name} = ${parameter.value}")
+
+                if (index < parameters.size - 1) {
+                    append(", ")
+                }
+            }
+
+            append(")")
         }
 
         append(" {\n")
--- src/commonMain/kotlin/com/chrynan/graphqlquerybuilder/ScalarGraphQLQueryFieldBuilder.kt
+++ src/commonMain/kotlin/com/chrynan/graphqlquerybuilder/ScalarGraphQLQueryFieldBuilder.kt
@@ -8,8 +8,18 @@ data class ScalarGraphQLQueryFieldBuilder(
     internal fun build(): String = buildString {
         append(name)
 
-        for (parameter in parameters) {
-            append("${parameter.name} = ${parameter.value}")
+        if (parameters.isNotEmpty()) {
+            append("(")
+
+            parameters.forEachIndexed { index, parameter ->
+                append("${parameter.name} = ${parameter.value}")
+
+                if (index < parameters.size - 1) {
+                    append(", ")
+                }
+            }
+
+            append(")")
         }
 
         append("\n")
